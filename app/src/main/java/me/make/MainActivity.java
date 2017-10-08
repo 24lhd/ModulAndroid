215836f13a863fb49004ba93e5a37d7ec834e05d
duong <24duong@gmail.com>
2017-10-08 18:30:12 +0700

    them cac module ui

--- app/src/main/java/me/make/MainActivity.java
+++ app/src/main/java/me/make/MainActivity.java
@@ -7,29 +7,33 @@ import android.util.Log;
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.Charset;
 
-import damanhinh.ChucNangPhu;
+import duong.ChucNangPhu;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        showAll(new ABC(),new ABC(),new ABC(),new ABC(),new ABC(),new ABC());
+        setContentView(R.layout.activity_main2);
+        showAll(new ABC(), new ABC(), new ABC(), new ABC(), new ABC(), new ABC());
         CharSequence replace = m1310a("e864702ca3c7602961a3c36020a3cc60236ab3c86070286e61aab1e84e2325fe236b9bb1e0b6c021a6cd602963aab1e8607023699bb1e572602164702e6fbab1e9686b60239bb1e86021a6cd60247daab1", "e2325f").replace("<br>", "\n");
-        Log.e("faker",replace.toString());
+        Log.e("faker", replace.toString());
     }
-    class  ABC{
+
+    class ABC {
 
     }
+
     private void showAll(ABC... aBC) {
-        Log.e("faker",""+aBC.length);
+        Log.e("faker", "" + aBC.length);
     }
 
     @Override
     public void onBackPressed() {
         ChucNangPhu.finishDoubleCick(this);
     }
+
     public String m1310a(String str, String str2) {
         int i = 0;
         if (str.indexOf(str2) < 0) {
